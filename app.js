3c5ad27f4caf2548faba5db4f61de549bbf10021
Gabriel Gamboa <gcarlos.gabriel4@gmail.com>
2023-06-25 23:29:22 -0400

    uuuultima hora

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ const express = require("express");
 const { create } = require("express-handlebars");
 const path = require("path");
 const morgan = require('morgan');
-const fs = require('fs');
 const cors = require('cors');
+const { exec } = require('child_process');
 const animeRutas = require('./router/anime_rutas.js');
 
 const app = express();
 
 //configuración handlebars
 const hbs = create({
- 
     partialsDir: ["views/partials/"],
 });
+
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 app.set("views", path.resolve(__dirname, "./views"));
@@ -24,11 +24,11 @@ app.use(morgan('short'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// middleware de precarga ( idea de marcelo <3 )
+// middleware de carga simulada ( idea de marcelo <3 )
 const delayMiddleware = (req, res, next) => setTimeout(next, 500);
 app.use(delayMiddleware);
 
-// cors
+// middleware para los errores cors
 app.use(cors());
 
 // compartir carpeta public
@@ -38,7 +38,11 @@ app.use(animeRutas);
 
 // iniciamos el servidor 
 const PORT = 3000
-app.listen(PORT, () =>
-    console.log("Servidor escuchando puerto:: "+PORT)
-);
+app.listen(PORT, () =>{
+    console.log(`Servidor iniciado e puerto ${PORT}.::`);
+    const command = process.platform === 'win32' ? 'start' : 'open';
+    exec(`${command} http://localhost:${PORT}/`);
+});
+
+
 
